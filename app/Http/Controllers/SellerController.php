cdbf03ab15b419065a6a5560139f8db0c2f29884
eacevedof <eacevedof@yahoo.es>
2018-08-23 16:58:27 +0200

    Controllers Buyer y Seller

---
+++ app/Http/Controllers/SellerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Seller;
+use Illuminate\Http\Request;
+
+class SellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller)
+    {
+        //
+    }
+}
