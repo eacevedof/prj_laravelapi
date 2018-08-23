3344effe3d427bbeeaa19022c2ab1fc23898127c
eacevedof <eacevedof@yahoo.es>
2018-08-23 21:30:07 +0200

    Configurando ProductFactory se necesita tocar el modelo Product ya que hay booleanos y se incluyen como  constantes

--- app/Product.php
+++ app/Product.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    const AVAILABLE = "available";
+    const NOT_AVAILABLE = "not available";
+    
     protected $fillable = [
         "name","description","quantity","status"
         ,"seller_id"
     ];
     
-    
-}
+}//Product
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,17 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->word,
+        "description" => $faker->paragraph(1),
+        "quantity" => $faker->numberBetween(1,10),
+        //mala práctica, mejor definirlos como constantes en el modelo
+        //"status" => $faker->randomElement(["disponible","no_disponible"]),
+        //"status" => $faker->randomElement(["si","no"]),
+        
+        "status" => $faker->randomElement([Product::AVAILABLE,Product::NOT_AVAILABLE]),
+        //de todos los usuarios obten uno y de ese id
+        "seller_id" => User::all()->random()->id,
+        //es lo mismo que el anterior
+        //"seller_id" => User::inRandomOrder()->first()->id,
     ];
 });
