f2ac4b2105e560d041c74e8ce3c18802b07a2d88
eacevedof <eacevedof@yahoo.es>
2018-08-23 17:07:16 +0200

    quito create y edit

--- app/Http/Controllers/BuyerController.php
+++ app/Http/Controllers/BuyerController.php
@@ -17,15 +17,6 @@ class BuyerController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -49,16 +40,6 @@ class BuyerController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Buyer  $buyer
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Buyer $buyer)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -18,16 +18,6 @@ class SellerController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -45,17 +35,6 @@ class SellerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Seller $seller)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Seller  $seller
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Seller $seller)
     {
         //
     }
