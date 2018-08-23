1e9b62ece1757da5c1a7c9ea38bb6f3c28e932d7
eacevedof <eacevedof@yahoo.es>
2018-08-23 21:31:35 +0200

    Factorias creadas. Son archivos que usan la global $factory y Faker para generar datos aleatorios en las tablas de los modelos

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->word,
+        "description" => $faker->paragraph(1),
     ];
 });
--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -1,9 +1,16 @@
 <?php
-
 use Faker\Generator as Faker;
 
 $factory->define(App\Transaction::class, function (Faker $faker) {
+    //No uso User pq no tiene relación directa con Transactions
+    //Como laravel sabe que Seller tiene Products?. 
+    //Esto se hace despues, al configurar las fks en los modelos
+    $seller = Seller::has("products")->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+    
     return [
-        //
+        "quantity" => $faker->numberBetween(1,3),
+        "buyer_id" => $buyer->id,
+        "product_id"=> $seller->products->random()->id,
     ];
 });
