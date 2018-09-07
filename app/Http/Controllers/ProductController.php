b19b655fc2c837944b7bf74feef0a61b1b568acb
eacevedof <eacevedof@yahoo.es>
2018-09-07 23:20:09 +0200

    Todos los controladores configurados con las operaciones básicas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,23 +9,22 @@ class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
-    }
+    {   
+        $oCollection = Product::all();
+        return $this->showAll($oCollection);
+    }//index
 
     /**
      * Display the specified resource.
-     *
      * @param  \App\Product  $product
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
     {
-        //
-    }
+        return $this->showOne($product);
+    }//show
     
 }//ProductController
--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,23 +9,21 @@ class TransactionController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $oCollection = Transaction::all();
         return $this->showAll($oCollection);
-    }
+    }//index
 
     /**
      * Display the specified resource.
-     *
      * @param  \App\Transaction  $transaction
      * @return \Illuminate\Http\Response
      */
     public function show(Transaction $transaction)
     {
         return $this->showOne($transaction);
-    }
+    }//show
 }//TransactionController
