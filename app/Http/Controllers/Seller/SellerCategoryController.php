7f437871d436fbfddc0eba2ccfc00c276a09b4ab
eacevedof <eacevedof@yahoo.es>
2018-09-15 16:13:28 +0200

    SellerCategoryController creado solo con index

---
+++ app/Http/Controllers/Seller/SellerCategoryController.php
@@ -0,0 +1,23 @@
+<?php
+//<project>/app/Http/Controllers/Seller/SellerCategoryController.php
+namespace App\Http\Controllers\Seller;
+
+use App\Category;
+use App\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SellerCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+
+    }
+
+}//SellerCategoryController
